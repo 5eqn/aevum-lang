937f0ddc0ee55fbcdd1de2c3de776c2a9d410995
5eqn <491100866@qq.com>
2023-05-27 18:35:35 +0800

    Change count script

--- count.sh
+++ count.sh
@@ -1,6 +1,10 @@
 a=`./build/counter/aevum src/Aevum/Main.idr`
 b=`./build/counter/aevum src/Aevum/Path.idr`
 c=`./build/counter/aevum src/Aevum/Util.idr`
-c=`./build/counter/aevum src/Aevum/Type.idr`
-let d=a+b+c
-echo $d
+d=`./build/counter/aevum src/Aevum/Type.idr`
+let e=a+b+c+d
+echo Total $e lines
+echo Main.idr $a lines
+echo Path.idr $b lines
+echo Util.idr $c lines
+echo Type.idr $d lines
